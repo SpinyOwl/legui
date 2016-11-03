b3b4bf1a300a2a25e8a19cbf477945099051dd8a
Aliaksandr_Shcherbin <Aliaksandr_Shcherbin@epam.com>
2016-11-03 19:13:03 +0300

    added Clipboard - paste functionality

--- src/main/java/org/liquidengine/legui/listener/system/textinput/TextInputSystemKeyEventProcessor.java
+++ src/main/java/org/liquidengine/legui/listener/system/textinput/TextInputSystemKeyEventProcessor.java
@@ -5,6 +5,9 @@ import org.liquidengine.legui.component.optional.TextState;
 import org.liquidengine.legui.context.LeguiContext;
 import org.liquidengine.legui.event.system.SystemKeyEvent;
 import org.liquidengine.legui.listener.SystemEventListener;
+import org.lwjgl.system.MemoryUtil;
+
+import java.nio.ByteBuffer;
 
 import static org.lwjgl.glfw.GLFW.*;
 
@@ -17,28 +20,42 @@ public class TextInputSystemKeyEventProcessor implements SystemEventListener<Tex
         if (gui.isFocused() && gui.isEditable()) {
             int key = event.key;
             int caretPosition = gui.getCaretPosition();
-            if (key == GLFW_KEY_LEFT && event.action != GLFW_RELEASE) {
+            boolean pressed = event.action != GLFW_RELEASE;
+            if (key == GLFW_KEY_LEFT && pressed) {
                 if (caretPosition > 0) {
                     gui.setCaretPosition(caretPosition - 1);
                 }
             } else {
                 TextState textState = gui.getTextState();
-                if (key == GLFW_KEY_RIGHT && event.action != GLFW_RELEASE) {
+                if (key == GLFW_KEY_RIGHT && pressed) {
                     if (caretPosition < textState.length()) {
                         gui.setCaretPosition(caretPosition + 1);
                     }
-                } else if ((key == GLFW_KEY_UP || key == GLFW_KEY_HOME) && event.action != GLFW_RELEASE) {
+                } else if ((key == GLFW_KEY_UP || key == GLFW_KEY_HOME) && pressed) {
                     gui.setCaretPosition(0);
-                } else if ((key == GLFW_KEY_DOWN || key == GLFW_KEY_END) && event.action != GLFW_RELEASE) {
+                } else if ((key == GLFW_KEY_DOWN || key == GLFW_KEY_END) && pressed) {
                     gui.setCaretPosition(gui.getTextState().length());
-                } else if (key == GLFW_KEY_BACKSPACE && event.action != GLFW_RELEASE) {
+                } else if (key == GLFW_KEY_BACKSPACE && pressed) {
                     if (caretPosition != 0) {
                         textState.deleteCharAt(caretPosition - 1);
                         gui.setCaretPosition(caretPosition - 1);
                     }
-                } else if (key == GLFW_KEY_DELETE && event.action != GLFW_RELEASE) {
+                } else if (key == GLFW_KEY_DELETE && pressed) {
                     if (caretPosition != textState.length()) {
                         textState.deleteCharAt(caretPosition);
+                    }
+                } else if (key == GLFW_KEY_V && pressed && event.mods == GLFW_MOD_CONTROL) {
+                    String s = glfwGetClipboardString(leguiContext.getGlfwWindow());
+                    if (s != null) {
+                        textState.insert(caretPosition, s);
+                        gui.setCaretPosition(caretPosition + s.length());
+                    } else {
+                        long l = nglfwGetClipboardString(leguiContext.getGlfwWindow());
+                        ByteBuffer byteBuffer = MemoryUtil.memByteBufferNT1(l);
+                        System.out.println(l + " " + (byteBuffer==null));
+                        if(byteBuffer!=null){
+                            System.out.println(byteBuffer.capacity());
+                        }
                     }
                 }
             }
