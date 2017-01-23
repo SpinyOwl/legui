99d16e00c322842d980d795f4bf217c265821694
Alexander Shcherbin <shchalexander@gmail.com>
2017-01-23 03:33:10 +0300

    added structure

---
+++ src/main/java/org/liquidengine/legui/exception/LeguiExceptionDefault.java
@@ -0,0 +1,8 @@
+package org.liquidengine.legui.exception;
+
+/**
+ * Enumeration of default exception messages
+ * Created by Alexander on 14.01.2017.
+ */
+public enum LeguiExceptionDefault {
+}
---
+++ src/main/java/org/liquidengine/legui/renderer/LeguiBorderRenderer.java
@@ -0,0 +1,9 @@
+package org.liquidengine.legui.renderer;
+
+/**
+ * Renderer interface for Legui Border.
+ * <p>
+ * Created by Alexander on 13.01.2017.
+ */
+public interface LeguiBorderRenderer {
+}
