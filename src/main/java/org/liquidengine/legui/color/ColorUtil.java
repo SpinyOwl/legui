2364090aee9ab2cabc107b7ca4592b7c351ad74c
Alexander Shcherbin <shchalexander@gmail.com>
2017-02-16 02:02:50 +0300

    docs

--- src/main/java/org/liquidengine/legui/color/ColorUtil.java
+++ src/main/java/org/liquidengine/legui/color/ColorUtil.java
@@ -5,7 +5,7 @@ import org.joml.Vector4f;
 import java.util.Random;
 
 /**
- * Created by ShchAlexander on 11.02.2017.
+ * Used to perform some operations with color vectors.
  */
 public final class ColorUtil {
     private static final Random RANDOM = new Random(System.currentTimeMillis());
@@ -13,36 +13,83 @@ public final class ColorUtil {
     private ColorUtil() {
     }
 
+    /**
+     * Used to create negative color for provided color.
+     * Alpha value will be the same as in source color.
+     *
+     * @param color color to negotiate.
+     * @return new negative color.
+     */
     public static Vector4f negativeColor(Vector4f color) {
         Vector4f dest = new Vector4f(1).sub(color);
         dest.w = color.w;
         return dest;
     }
 
+    /**
+     * Used to create negative color and set it to dest.
+     * Alpha value will be the same as in source color.
+     *
+     * @param color color to negotiate.
+     * @param dest  target color object to set.
+     */
     public static void negativeColor(Vector4f color, Vector4f dest) {
         dest.zero().set(1).sub(color);
         dest.w = color.w;
     }
 
+    /**
+     * Used to negotiate only rgb part.
+     * Alpha value will be setted to 1.
+     *
+     * @param color color to negotiate.
+     * @return new negative color.
+     */
     public static Vector4f negativeColorRGB(Vector4f color) {
         Vector4f dest = new Vector4f(1).sub(color);
         dest.w = 1;
         return dest;
     }
 
+    /**
+     * Used to create negative color and set it to dest.
+     * Alpha value will be setted to 1.
+     *
+     * @param color color to negotiate.
+     * @param dest  target color object to set.
+     */
     public static void negativeColorRGB(Vector4f color, Vector4f dest) {
         dest.zero().set(1).sub(color);
         dest.w = 1;
     }
 
+    /**
+     * Used to obtain half color from source.
+     *
+     * @param color source color.
+     * @return new color.
+     */
     public static Vector4f half(Vector4f color) {
         return new Vector4f(color).div(2);
     }
 
+    /**
+     * Used to return black or white color based on source color. for example for white source color it should return black.
+     *
+     * @param color color to find opposite black or white color.
+     * @return opposite black or white color.
+     */
     public static Vector4f oppositeBlackOrWhite(Vector4f color) {
         return oppositeBlackOrWhite(color, new Vector4f(1));
     }
 
+    /**
+     * Used to return black or white color based on source color. for example for white source color it should return black.
+     *
+     * @param color       color to find opposite black or white color.
+     * @param targetColor color to set result.
+     * @return opposite black or white color.
+     */
     public static Vector4f oppositeBlackOrWhite(Vector4f color, Vector4f targetColor) {
         if (color.w == 0) {
             return ColorConstants.black();
@@ -59,6 +106,11 @@ public final class ColorUtil {
         return targetColor;
     }
 
+    /**
+     * Used to generate random color.
+     *
+     * @return random color.
+     */
     public static Vector4f randomColor() {
         return new Vector4f(RANDOM.nextFloat(), RANDOM.nextFloat(), RANDOM.nextFloat(), 1);
     }
