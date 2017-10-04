d676c68c94fa15020956381965bd3238dc429f9b
ShchAlexander <shchalexander@gmail.com>
2017-10-04 18:19:26 +0300

    Added fields to TextInputContentChangeEvent of old and new text values.

--- src/main/java/org/liquidengine/legui/component/event/textinput/TextInputContentChangeEvent.java
+++ src/main/java/org/liquidengine/legui/component/event/textinput/TextInputContentChangeEvent.java
@@ -10,7 +10,36 @@ import org.liquidengine.legui.system.context.Context;
  */
 public class TextInputContentChangeEvent<T extends TextInput> extends Event<T> {
 
+    /**
+     * Old value.
+     */
+    private final String oldValue;
+    /**
+     * New value.
+     */
+    private final String newValue;
+
     public TextInputContentChangeEvent(T component, Context context, Frame frame, String oldValue, String newValue) {
         super(component, context, frame);
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+    }
+
+    /**
+     * Returns old value.
+     *
+     * @return old value.
+     */
+    public String getOldValue() {
+        return oldValue;
+    }
+
+    /**
+     * Returns new value.
+     *
+     * @return new value.
+     */
+    public String getNewValue() {
+        return newValue;
     }
 }
