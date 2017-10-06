aabae36083c296af3187a87719a0b22162d678f9
ShchAlexander <shchalexander@gmail.com>
2017-10-06 18:26:33 +0300

    Added javadocs for SystemEventProcessor class

--- src/main/java/org/liquidengine/legui/system/handler/processor/SystemEventProcessor.java
+++ src/main/java/org/liquidengine/legui/system/handler/processor/SystemEventProcessor.java
@@ -46,6 +46,12 @@ public class SystemEventProcessor {
 
     private Queue<SystemEvent> eventQueue = new ConcurrentLinkedQueue<>();
 
+    /**
+     * Process events.
+     *
+     * @param frame the frame
+     * @param context the context
+     */
     public void processEvents(Frame frame, Context context) {
         for (SystemEvent event = eventQueue.poll(); event != null; event = eventQueue.poll()) {
             SystemEventHandler processor = SystemEventHandlerProvider.getInstance().getProcessor(event.getClass());
@@ -55,11 +61,21 @@ public class SystemEventProcessor {
         }
     }
 
+    /**
+     * Push event.
+     *
+     * @param event the event
+     */
     public void pushEvent(SystemEvent event) {
         eventQueue.add(event);
     }
 
 
+    /**
+     * Add default callbacks to callback keeper.
+     *
+     * @param guiCallbackKeeper the gui callback keeper
+     */
     public void addDefaultCallbacks(CallbackKeeper guiCallbackKeeper) {
         //@formatter:off
         guiCallbackKeeper.getChainCharCallback()            .add(createDefaultGlfwCharCallbackI());
@@ -80,62 +96,137 @@ public class SystemEventProcessor {
         //@formatter:on
     }
 
+    /**
+     * Creates default GLFWWindowSizeCallback.
+     *
+     * @return the GLFWWindowSizeCallback.
+     */
     public GLFWWindowSizeCallbackI createDefaultGlfwWindowSizeCallbackI() {
         return (window, width, height) -> eventQueue.add(new SystemWindowSizeEvent(window, width, height));
     }
 
+    /**
+     * Creates default GLFWWindowRefreshCallback.
+     *
+     * @return the GLFWWindowRefreshCallback.
+     */
     public GLFWWindowRefreshCallbackI createDefaultGlfwWindowRefreshCallbackI() {
         return window -> eventQueue.add(new SystemWindowRefreshEvent(window));
     }
 
+    /**
+     * Creates default GLFWWindowPosCallback.
+     *
+     * @return the GLFWWindowPosCallback.
+     */
     public GLFWWindowPosCallbackI createDefaultGlfwWindowPosCallbackI() {
         return (window, xpos, ypos) -> eventQueue.add(new SystemWindowPosEvent(window, xpos, ypos));
     }
 
+    /**
+     * Creates default GLFWWindowIconifyCallback.
+     *
+     * @return the GLFWWindowIconifyCallback.
+     */
     public GLFWWindowIconifyCallbackI createDefaultGlfwWindowIconifyCallbackI() {
         return (window, iconified) -> eventQueue.add(new SystemWindowIconifyEvent(window, iconified));
     }
 
+    /**
+     * Creates default GLFWWindowFocusCallback.
+     *
+     * @return the GLFWWindowFocusCallback.
+     */
     public GLFWWindowFocusCallbackI createDefaultGlfwWindowFocusCallbackI() {
         return (window, focused) -> eventQueue.add(new SystemWindowFocusEvent(window, focused));
     }
 
+    /**
+     * Creates default GLFWWindowCloseCallback.
+     *
+     * @return the GLFWWindowCloseCallback.
+     */
     public GLFWWindowCloseCallbackI createDefaultGlfwWindowCloseCallbackI() {
         return (window) -> eventQueue.add(new SystemWindowCloseEvent(window));
     }
 
+    /**
+     * Creates default GLFWCursorPosCallback.
+     *
+     * @return the GLFWCursorPosCallback.
+     */
     public GLFWCursorPosCallbackI createDefaultGlfwCursorPosCallbackI() {
         return (window, xpos, ypos) -> eventQueue.add(new SystemCursorPosEvent(window, xpos, ypos));
     }
 
+    /**
+     * Creates default GLFWMouseButtonCallback.
+     *
+     * @return the GLFWMouseButtonCallback.
+     */
     public GLFWMouseButtonCallbackI createDefaultGlfwMouseButtonCallbackI() {
         return (window, button, action, mods) -> eventQueue.add(new SystemMouseClickEvent(window, button, action, mods));
     }
 
+    /**
+     * Creates default GLFWFramebufferSizeCallback.
+     *
+     * @return the GLFWFramebufferSizeCallback.
+     */
     public GLFWFramebufferSizeCallbackI createDefaultGlfwFramebufferSizeCallbackI() {
         return (window, width, height) -> eventQueue.add(new SystemFramebufferSizeEvent(window, width, height));
     }
 
+    /**
+     * Creates default GLFWCursorEnterCallback.
+     *
+     * @return the GLFWCursorEnterCallback.
+     */
     public GLFWCursorEnterCallbackI createDefaultGlfwCursorEnterCallbackI() {
         return (window, entered) -> eventQueue.add(new SystemCursorEnterEvent(window, entered));
     }
 
+    /**
+     * Creates default GLFWCharModsCallback.
+     *
+     * @return the GLFWCharModsCallback.
+     */
     public GLFWCharModsCallbackI createDefaultGlfwCharModsCallbackI() {
         return (window, codepoint, mods) -> eventQueue.add(new SystemCharModsEvent(window, codepoint, mods));
     }
 
+    /**
+     * Creates default GLFWScrollCallback.
+     *
+     * @return the GLFWScrollCallback.
+     */
     public GLFWScrollCallbackI createDefaultGlfwScrollCallbackI() {
         return (window, xoffset, yoffset) -> eventQueue.add(new SystemScrollEvent(window, xoffset, yoffset));
     }
 
+    /**
+     * Creates default GLFWKeyCallback.
+     *
+     * @return the GLFWKeyCallback.
+     */
     public GLFWKeyCallbackI createDefaultGlfwKeyCallbackI() {
         return (window, key, scancode, action, mods) -> eventQueue.add(new SystemKeyEvent(window, key, scancode, action, mods));
     }
 
+    /**
+     * Creates default GLFWDropCallback.
+     *
+     * @return the GLFWDropCallback.
+     */
     public GLFWDropCallbackI createDefaultGlfwDropCallbackI() {
         return (window, count, names) -> eventQueue.add(new SystemDropEvent(window, count, names));
     }
 
+    /**
+     * Creates default GLFWCharCallback.
+     *
+     * @return the GLFWCharCallback.
+     */
     public GLFWCharCallbackI createDefaultGlfwCharCallbackI() {
         return (window, codepoint) -> eventQueue.add(new SystemCharEvent(window, codepoint));
     }
