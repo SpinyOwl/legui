7c52c43f7f882e3458be72fa13e3635d051f4366
ShchAlexander <shchalexander@gmail.com>
2017-10-06 18:23:51 +0300

    Added javadocs for Context class

--- src/main/java/org/liquidengine/legui/system/context/Context.java
+++ src/main/java/org/liquidengine/legui/system/context/Context.java
@@ -33,22 +33,45 @@ public class Context {
 
     private Map<String, Object> contextData = new ConcurrentHashMap<>();
 
+    /**
+     * Instantiates a new Context.
+     *
+     * @param glfwWindow the glfw window
+     */
     public Context(long glfwWindow) {
         this.glfwWindow = glfwWindow;
     }
 
+    /**
+     * Is debug enabled boolean.
+     *
+     * @return the boolean
+     */
     public boolean isDebugEnabled() {
         return debugEnabled;
     }
 
+    /**
+     * Sets debug enabled.
+     *
+     * @param debugEnabled the debug enabled
+     */
     public void setDebugEnabled(boolean debugEnabled) {
         this.debugEnabled = debugEnabled;
     }
 
+    /**
+     * Gets context data.
+     *
+     * @return the context data
+     */
     public Map<String, Object> getContextData() {
         return contextData;
     }
 
+    /**
+     * Update glfw window.
+     */
     public void updateGlfwWindow() {
         int[] windowWidth = {0},
             windowHeight = {0};
@@ -67,6 +90,17 @@ public class Context {
         );
     }
 
+    /**
+     * Update.
+     *
+     * @param targetWidth the target width
+     * @param targetHeight the target height
+     * @param framebufferWidth the framebuffer width
+     * @param framebufferHeight the framebuffer height
+     * @param targetPosX the target pos x
+     * @param targetPosY the target pos y
+     * @param iconified the iconified
+     */
     public void update(int targetWidth, int targetHeight, int framebufferWidth, int framebufferHeight,
         int targetPosX, int targetPosY, boolean iconified) {
         setWindowSize(new Vector2i(targetWidth, targetHeight));
@@ -76,62 +110,137 @@ public class Context {
         setIconified(iconified);
     }
 
+    /**
+     * Gets pixel ratio.
+     *
+     * @return the pixel ratio
+     */
     public float getPixelRatio() {
         return pixelRatio;
     }
 
+    /**
+     * Sets pixel ratio.
+     *
+     * @param pixelRatio the pixel ratio
+     */
     public void setPixelRatio(float pixelRatio) {
         this.pixelRatio = pixelRatio;
     }
 
+    /**
+     * Gets glfw window.
+     *
+     * @return the glfw window
+     */
     public long getGlfwWindow() {
         return glfwWindow;
     }
 
+    /**
+     * Gets window position.
+     *
+     * @return the window position
+     */
     public Vector2f getWindowPosition() {
         return windowPosition;
     }
 
+    /**
+     * Sets window position.
+     *
+     * @param windowPosition the window position
+     */
     public void setWindowPosition(Vector2f windowPosition) {
         this.windowPosition = windowPosition;
     }
 
+    /**
+     * Gets window size.
+     *
+     * @return the window size
+     */
     public Vector2i getWindowSize() {
         return windowSize;
     }
 
+    /**
+     * Sets window size.
+     *
+     * @param windowSize the window size
+     */
     public void setWindowSize(Vector2i windowSize) {
         this.windowSize = windowSize;
     }
 
+    /**
+     * Gets framebuffer size.
+     *
+     * @return the framebuffer size
+     */
     public Vector2f getFramebufferSize() {
         return framebufferSize;
     }
 
+    /**
+     * Sets framebuffer size.
+     *
+     * @param framebufferSize the framebuffer size
+     */
     public void setFramebufferSize(Vector2f framebufferSize) {
         this.framebufferSize = framebufferSize;
     }
 
+    /**
+     * Gets focused gui.
+     *
+     * @return the focused gui
+     */
     public Component getFocusedGui() {
         return focusedGui;
     }
 
+    /**
+     * Sets focused gui.
+     *
+     * @param focusedGui the focused gui
+     */
     public void setFocusedGui(Component focusedGui) {
         this.focusedGui = focusedGui;
     }
 
+    /**
+     * Returns current mouse target component.
+     *
+     * @return current mouse target component.
+     */
     public Component getMouseTargetGui() {
         return mouseTargetGui;
     }
 
+    /**
+     * Used to update current mouse target component.
+     *
+     * @param mouseTargetGui new mouse target component.
+     */
     public void setMouseTargetGui(Component mouseTargetGui) {
         this.mouseTargetGui = mouseTargetGui;
     }
 
+    /**
+     * Returns window iconified state.
+     *
+     * @return window iconified state.
+     */
     public boolean isIconified() {
         return iconified;
     }
 
+    /**
+     * Used to update state of window (in case of window iconified).
+     *
+     * @param iconified window state.
+     */
     public void setIconified(boolean iconified) {
         this.iconified = iconified;
     }
