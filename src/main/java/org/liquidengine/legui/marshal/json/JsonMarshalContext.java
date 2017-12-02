18484589d69cd568a308430ad190d614ce5e67d2
Alexander Shcherbin <shchalexander@gmail.com>
2017-12-02 17:29:03 +0300

    #64: Removed old marshallers

--- src/main/java/org/liquidengine/legui/marshal/json/JsonMarshalContext.java
+++
@@ -1,8 +0,0 @@
-package org.liquidengine.legui.marshal.json;
-
-/**
- * Marshal context which used by marshaller. Created by Aliaksandr_Shcherbin on 2/24/2017.
- */
-public interface JsonMarshalContext {
-
-}
