d056bfc4df262bd92ac9f608955dc50c91fe61e7
ShchAlexander <shchalexander@gmail.com>
2020-03-07 19:21:27 +0300

    added indexOf method to Component

--- src/main/java/org/liquidengine/legui/component/Component.java
+++ src/main/java/org/liquidengine/legui/component/Component.java
@@ -823,4 +823,7 @@ public abstract class Component implements Serializable {
                 .toString();
     }
 
+    public int indexOfChild(Component component) {
+        return childComponents.indexOf(component);
+    }
 }
