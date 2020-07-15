8b2d2dd6a5d0595b8db2b5f3185e4676a7ab70f9
Alexander Shcherbin <shchalexander@gmail.com>
2020-07-15 03:08:06 +0300

    Fixed issue with FBOImage renderer

--- src/main/java/org/liquidengine/legui/system/renderer/nvg/image/NvgFBOImageRenderer.java
+++ src/main/java/org/liquidengine/legui/system/renderer/nvg/image/NvgFBOImageRenderer.java
@@ -16,7 +16,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.function.IntFunction;
 
-import static org.liquidengine.legui.system.renderer.nvg.NvgRenderer.IMAGE_REFERENCE_MANAGER;
 import static org.lwjgl.opengl.GL11.glGetInteger;
 import static org.lwjgl.opengl.GL30.GL_MAJOR_VERSION;
 import static org.lwjgl.opengl.GL30.GL_MINOR_VERSION;
@@ -77,7 +76,7 @@ public class NvgFBOImageRenderer extends NvgImageRenderer<FBOImage> {
     @Override
     protected void renderImage(FBOImage image, Vector2fc position, Vector2fc size, Map<String, Object> properties, Context context, long nanovg) {
 
-        NvgImageReferenceManager manager = (NvgImageReferenceManager) context.getContextData().get(IMAGE_REFERENCE_MANAGER);
+        NvgImageReferenceManager manager = NvgImageReferenceManager.getInstance();
         int imageRef = manager.getImageReference(image, nanovg);
 
         renderImage(imageRef, position, size, properties, nanovg);
